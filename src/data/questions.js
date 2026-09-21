5412cf2fc72cf608bb3ca008579c78fad8b9c7a4
stenogpk <stenogpk@gmail.com>
2026-09-21 21:51:57 +0530

    feat: add mission-specific marking configuration

--- src/data/questions.js
+++ src/data/questions.js
@@ -25,7 +25,7 @@ export const attemptSchema = {
   attemptedAt: 0,
 };
 
-export function calculateMarks(isCorrect, marking = { correct: 1, wrong: 0 }) {
+export const missionMarking = {\n  pcs: null,\n  chemistry: { correct: 3, wrong: -1 },\n};\n\nexport function calculateMarks(isCorrect, marking = { correct: 1, wrong: 0 }) {
   return isCorrect ? marking.correct : marking.wrong;
 }
 
